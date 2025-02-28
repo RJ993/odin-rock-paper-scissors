3041eb7f15cbaf2d411337d91e7f814ee50e6e19
RJ993 <196080894+RJ993@users.noreply.github.com>
2025-02-28 14:12:32 -0500

    Make score increment

--- triangle.js
+++ triangle.js
@@ -36,29 +36,45 @@ function getHumanChoice(){
     return humResult2
 }
 
-function playGame(){
 let humanScore = 0
 let computerScore = 0
 
+function playGame(){
     function playRound(humanChoice, computerChoice) {
     if ((humanChoice == "rock" && computerChoice === 2) || (humanChoice == "scissors" && computerChoice === 1) || (humanChoice == "paper" && computerChoice === 0)){
-        console.log("You win!")
-         humanScore++
+        console.log("You win this round!")
+        humanScore += 1
     }else if((humanChoice == "scissors" && computerChoice === 0) || (humanChoice == "rock" && computerChoice === 1) || (humanChoice == "paper" && computerChoice === 2)){
-        console.log("You lose...")
-         computerScore++
+        console.log("You lose this round...")
+        computerScore += 1
     }else if((humanChoice == "scissors" && computerChoice === 2) || (humanChoice == "rock" && computerChoice === 0) || (humanChoice == "paper" && computerChoice === 1)){
-        console.log("Tie!")
+        console.log("Round tied.")
+        humanScore += 1
+        computerScore += 1
     }
+    return humanScore && computerScore
   }
   
     const humanSelection = getHumanChoice();
     const computerSelection = getComputerChoice();
   
         playRound(humanSelection, computerSelection);
-        
+        console.log("You - " + humanScore, "Computer - " + computerScore)
+        return humanScore && computerScore
 }
 
 for (let i = 0; i < 5; i++){
     playGame()
 }
+
+function declareWinner(){
+    if (humanScore > computerScore){
+        console.log("You win!")
+    }else if (humanScore < computerScore){
+        console.log("You lost...")
+    }else if (humanScore === computerScore){
+        console.log ("Tie!")
+    }
+}
+
+declareWinner()
