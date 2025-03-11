43e7dbaf7ea4b447bbe5fef8661f5718e016d0f1
RJ993 <196080894+RJ993@users.noreply.github.com>
2025-03-11 16:54:50 -0400

    Add functional buttons to read input

--- triangle.js
+++ triangle.js
@@ -1,46 +1,16 @@
-/* 
-Make getComputerChoice
-    Randomize the outcomes (outcomes should have EQUAL chance) of rock, paper, scissors with Math.random.
-        We do that by assigning the results values less than 3 and make math random round down.
-Make getHumanChoice
-    Prompt the user
-    User enters rock, paper, or scissors
-    The entry shows in the console log
-    The entry is case-insensitive
-    Any other entry will not be entered into console log.
-For the scoring system
-    Give it an initial value of zero
-    Make it increment by 1 per win
-*/
-
 function getComputerChoice(){
     let compResult = Math.floor(Math.random()*3)
     if (compResult === 0){
-        console.log("rock")
     }else if (compResult === 1){
-        console.log("paper")
     }else if (compResult === 2){
-        console.log("scissors")
     }
     return compResult
-}
-
-function getHumanChoice(){
-    let humResult1 = prompt("Rock, Paper, or Scissors?", "")
-    let humResult2 = humResult1.toLowerCase("")
-    if (humResult2 == "rock" || humResult2 == "paper" || humResult2 == "scissors"){
-        console.log(humResult2)
-    }else{
-        alert("Please refresh the page!")
-    }
-    return humResult2
 }
 
 let humanScore = 0
 let computerScore = 0
 
-function playGame(){
-    function playRound(humanChoice, computerChoice) {
+function playRound(humanChoice, computerChoice) {
     if ((humanChoice == "rock" && computerChoice === 2) || (humanChoice == "scissors" && computerChoice === 1) || (humanChoice == "paper" && computerChoice === 0)){
         console.log("You win this round!")
         humanScore += 1
@@ -49,31 +19,41 @@ function playGame(){
         computerScore += 1
     }else if((humanChoice == "scissors" && computerChoice === 2) || (humanChoice == "rock" && computerChoice === 0) || (humanChoice == "paper" && computerChoice === 1)){
         console.log("Round tied.")
-        humanScore += 1
-        computerScore += 1
+        humanScore += 0
+        computerScore += 0
+
     }
     return humanScore && computerScore
-  }
+  } 
   
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-  
-        playRound(humanSelection, computerSelection);
-        console.log("You - " + humanScore, "Computer - " + computerScore)
-        return humanScore && computerScore
-}
+    const humanSelection = ['rock', 'paper', 'scissors']
 
-/* for (let i = 0; i < 5; i++){
-    playGame()
-} */
+        const rock = document.querySelector("#rock")
+        const paper = document.querySelector("#paper")
+        const scissors = document.querySelector("#scissors")
+
+
+        rock.addEventListener("click", function (e){
+            playRound(humanSelection.at(0), getComputerChoice())
+            console.log("You - " + humanScore, "Computer - " + computerScore)
+            return humanScore && computerScore
+        })
+        paper.addEventListener("click", function (e){
+            playRound(humanSelection.at(1), getComputerChoice())
+            console.log("You - " + humanScore, "Computer - " + computerScore)
+            return humanScore && computerScore
+        })
+        scissors.addEventListener("click", function (e){
+            playRound(humanSelection.at(2), getComputerChoice())
+            console.log("You - " + humanScore, "Computer - " + computerScore)
+            return humanScore && computerScore
+        })
 
 function declareWinner(){
     if (humanScore > computerScore){
         console.log("You win!")
     }else if (humanScore < computerScore){
         console.log("You lost...")
-    }else if (humanScore === computerScore){
-        console.log ("Tie!")
     }
 }
 
